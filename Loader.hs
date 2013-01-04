027c747cc92fb2032aae7d584c8b1a21cd541124
Yotam Barnoy <yotambarnoy@gmail.com>
2013-01-04 12:48:23 -0500

    Loader is now case-insensitive

--- Loader.hs
+++ Loader.hs
@@ -17,20 +17,25 @@ import qualified Data.ByteString.Lazy as B
 import Codec.Archive.LibZip
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified Data.Char as C
+
+lowerCase s = map C.toLower s
 
 gamePath = "/Applications/Games/Din's Curse/Assets/"
 
 type DirName = String
 type FileName = String
+type ZipFilePath = String
 
-data Directory = Directory DirName (M.Map FileName (Maybe FilePath)) [Directory]
+-- (lowercaseName, (regularName, Maybe ZipPath)
+data Directory = Directory DirName (M.Map String (FileName, Maybe FilePath)) [Directory]
     deriving Show
 
 instance Eq Directory where
-    (Directory a _ _) == (Directory b _ _) = a == b
+    (Directory a _ _) == (Directory b _ _) = lowerCase a == lowerCase b
 
 instance Ord (Directory) where
-    compare (Directory a _ _) (Directory b _ _) = compare a b
+    compare (Directory a _ _) (Directory b _ _) = compare (lowerCase a) (lowerCase b)
 
 filesInZip n = do
     files <- withArchive [] n $ fileNames []
@@ -59,7 +64,6 @@ fullFileList = do
     dirContents <- getDirectoryContents gamePath
     let zipFiles = L.sort $ filter isZip dirContents
     zipFiles' <- mapM filesInZipPerFile zipFiles
-    {-zipFiles' <- mapM (wrapMaybeIO filesInZip . (gamePath++)) ["Assets002.zip"]-}
     let nonZipFiles = filter (not . isHidden) $ filter (not . isZip) dirContents
     let nonZipFiles' = map (\f -> (f, Nothing)) nonZipFiles 
     return $ (concat zipFiles') ++ nonZipFiles' 
@@ -72,11 +76,12 @@ splitPath path     = case L.elemIndex '/' path of
                    '/':xs -> (first, xs)
                    _      -> (first, last)
 
+
 addToDir (Directory n fileMap ds) (path, value) = 
     case splitPath path of
         ([],   _)      -> error $ "addToDir: Error splitting the path " ++ path
         (file, [])     -> Directory n newFM ds
-                              where newFM = M.insert file value fileMap
+                              where newFM = M.insert (lowerCase file) (file, value) fileMap
         (newDir, rest) -> 
             let (d, notDs) = L.partition (\(Directory n _ _) -> n == newDir) ds
             in case d of
@@ -85,44 +90,43 @@ addToDir (Directory n fileMap ds) (path, value) =
                 [d'] -> Directory n fileMap $ L.insert (addToDir d' (rest, value)) notDs
                 _    -> error $ "Too many directories " ++ newDir ++ " found"
 
-subdir (Directory _ _ ds) name = L.find (\(Directory n _ _) -> n == name) ds
+subdir (Directory _ _ ds) name = L.find (\(Directory n _ _) -> lowerCase n == lowerCase name) ds
 
--- return info about file
-data PathType = DirPathType Directory
-               | FilePathType Directory
-               | ZipPathType Directory FilePath
+-- return info about file/dir. We return the Directory & proper case name
+data PathType = DirPathType Directory FilePath
+               | FilePathType Directory FilePath 
+               | ZipPathType Directory FilePath ZipFilePath
                | NotFoundPathType
+               deriving Show
 
 findPath :: Directory -> FileName -> PathType
-findPath d [] = DirPathType d -- return directory only
-findPath d@(Directory n fileMap ds) path =
-    case splitPath path of
-        ([], _)      -> error $ "findPath: Error splitting the path " ++ path
-        (name, rest) -> case M.lookup name fileMap of
-                            Just (Just v)  -> ZipPathType d v -- found the file, return the archive
-                            Just Nothing   -> FilePathType d
-                            Nothing        -> case subdir d name of 
-                                                Just dir -> findPath dir rest
+findPath d path = inner d path []
+  where inner d@(Directory n _ _) [] accPath  = DirPathType d $ join accPath n
+        inner d@(Directory n fileMap ds) path accP =
+          let accPath = join accP n
+          in case splitPath path of
+            ([], _)      -> error $ "findPath: Error splitting the path " ++ path
+            (name, rest) -> case M.lookup (lowerCase name) fileMap of
+                              Just (file, Just v)  -> ZipPathType d (join accPath file) v
+                              Just (file, Nothing) -> FilePathType d $ join accPath file
+                              Nothing -> case subdir d name of 
+                                                Just dir -> inner dir rest accPath
                                                 Nothing  -> NotFoundPathType
+        join "" y = y
+        join x y = x ++ "/" ++ y
 
-{-readPath :: Directory -> String -> IO String-}
 readPath d path = case findPath d path of
-        ZipPathType _ zpath -> do
-            bytes <- withArchive [] (gamePath ++ zpath) $ fileContents [] path
+        ZipPathType _ casePath zpath -> do
+            bytes <- withArchive [] (gamePath ++ zpath) $ fileContents [] casePath
             let bytes' = B.pack bytes
             return bytes'
-        FilePathType _ -> do
-            bytes <- B.readFile $ gamePath ++ path
+        FilePathType _ casePath -> do
+            bytes <- B.readFile $ gamePath ++ casePath
             return bytes
-        DirPathType _    -> ioError $ userError $ "readFile: can't read directory " ++ path
+        DirPathType _ _  -> ioError $ userError $ "readFile: can't read directory " ++ path
         NotFoundPathType -> ioError $ userError $ "readFile: file " ++ path ++ " not found"
 
-directoryMapOfFileList fl = L.foldl' addToDir (Directory "." M.empty []) fl
-
-{-fullFileMap :: IO (M.Map String (Maybe FilePath))-}
-{-fullFileMap = do-}
-    {-list <- fullFileList-}
-    {-return $ M.fromList list-}
+directoryMapOfFileList fl = L.foldl' addToDir (Directory "" M.empty []) fl
 
 fullFileMap :: IO Directory
 fullFileMap = do
